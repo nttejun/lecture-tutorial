68acca560d0d87fd6a279dd8f122f97768930f77
Wonjun Jeong <nttejun@gmail.com>
2018-08-06 22:47:08 +0900

    Added http message converter request data to receiving object

--- springboot-mvc/src/main/java/spring/mvc/basic/user/UserController.java
+++ springboot-mvc/src/main/java/spring/mvc/basic/user/UserController.java
@@ -1,13 +1,24 @@
 package spring.mvc.basic.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(){
+        // @RestController 통해 "hello"는 View Resolver 에서 Message Converter로 인식
         return "hello";
     }
+
+    @PostMapping("/users/create")
+    // @RestController 제공하는 HttpMessageConverter
+    // Content-type, 요청본문 타입이 JSON 일 때 요청 데이터를 User 객체로 컨버트 되어진다 (=HttpMessageConverter)
+    public User create(@RequestBody User user){
+        return user;
+    }
+
 }
--- springboot-mvc/src/test/java/spring/mvc/basic/user/UserControllerTest.java
+++ springboot-mvc/src/test/java/spring/mvc/basic/user/UserControllerTest.java
@@ -4,11 +4,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.print.attribute.standard.Media;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -24,5 +31,17 @@ public class UserControllerTest {
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
+    }
+
+    @Test
+    public void createUser_JSON() throws Exception{
+        String userJson = "{\"username\":\"wonjun\", \"password\":\"1234\"}";
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username", is(equalTo("wonjun"))))
+                .andExpect(jsonPath("$.password", is(equalTo("1234"))));
     }
 }
