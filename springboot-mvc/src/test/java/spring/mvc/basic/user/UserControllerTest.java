a30c47694be33bb9d56da3930a1ec07b2e8c0c00
Wonjun Jeong <nttejun@gmail.com>
2018-08-06 21:28:43 +0900

    Added spring boot mvc test @WebMvcTest

---
+++ springboot-mvc/src/test/java/spring/mvc/basic/user/UserControllerTest.java
@@ -0,0 +1,28 @@
+package spring.mvc.basic.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+// DispatcherServlet 같은 설정 없이도 MVC 테스트가 가능
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception{
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
+}
