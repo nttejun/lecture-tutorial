80232460c45b826c101940c12e3a7df0caab75dc
Wonjun Jeong <nttejun@gmail.com>
2018-08-06 23:02:20 +0900

    Added spring boot view resolve function

--- springboot-mvc/src/test/java/spring/mvc/basic/user/UserControllerTest.java
+++ springboot-mvc/src/test/java/spring/mvc/basic/user/UserControllerTest.java
@@ -17,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
 @RunWith(SpringRunner.class)
 // DispatcherServlet 같은 설정 없이도 MVC 테스트가 가능
@@ -33,6 +34,23 @@ public class UserControllerTest {
                 .andExpect(content().string("hello"));
     }
 
+    // Spring boot ViewResolve 역할은 View 전체를 찾고 View 타입과 accept 헤더 타입과 동일한 타입을 선정한다
+    @Test
+    public void createUser_XML() throws Exception{
+        String userJson = "{\"username\":\"wonjun\", \"password\":\"1234\"}";
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_XML)
+                .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/User/name")
+                        .string("wonjun"))
+                .andExpect(xpath("/User/password")
+                        .string("1234"));
+    }
+
+
+    // Json 타입 요청을 Json 타입으로 받는 방법
     @Test
     public void createUser_JSON() throws Exception{
         String userJson = "{\"username\":\"wonjun\", \"password\":\"1234\"}";
@@ -44,4 +62,5 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.username", is(equalTo("wonjun"))))
                 .andExpect(jsonPath("$.password", is(equalTo("1234"))));
     }
+
 }
