46e37cd7263db7cf38d272367ca0d9fe0dbac94d
Wonjun Jeong <nttejun@gmail.com>
2018-08-07 23:40:26 +0900

    Add set static resource path and addResouceHandler

--- springboot-mvc/src/main/java/spring/mvc/basic/config/WebConfig.java
+++ springboot-mvc/src/main/java/spring/mvc/basic/config/WebConfig.java
@@ -1,9 +1,18 @@
 package spring.mvc.basic.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+// 원하는 리소스 핸들러 추가가 가능하다
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/p/**")
+                .addResourceLocations("classpath:/p/")
+                .setCachePeriod(20);
+    }
 }
