1068ebc041fd482feae94f195ec85499be51f87f
Wonjun Jeong <nttejun@gmail.com>
2018-09-02 19:49:30 +0900

    Added JPQL example

--- springboot-jpql/src/main/java/jpql/boot/persistence/BoardRepository.java
+++ springboot-jpql/src/main/java/jpql/boot/persistence/BoardRepository.java
@@ -1,14 +1,43 @@
 package jpql.boot.persistence;
 
 import jpql.boot.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface BoardRepository extends CrudRepository <Board, Long> {
 
-    public List<Board> findBoardByTitle(String title);
-    public Collection<Board>findByWriter(String writer);
+    List<Board> findBoardByTitle(String title);
+
+    Collection<Board>findByWriter(String writer);
+
+    Collection<Board>findByWriterContaining(String writer);
+
+    Collection<Board> findByWriterContainingOrTitleContaining(String writer, String title);
+
+    Collection<Board> findByTitleContainingAndBnoGreaterThan(String title, long bno);
+
+    Collection<Board> findByBnoGreaterThanOrderByBnoDesc(long bno);
+
+    List<Board> findByBnoGreaterThanOrderByBnoDesc(long bno, Pageable pageable);
+
+    Page<Board> findByBnoGreaterThan(long l, Pageable paging);
+
+    @Query("SELECT b FROM Board b WHERE b.title LIKE %?1% AND b.bno > 0 ORDER BY b.bno DESC")
+    List<Board> findByTitle(String title);
+
+    @Query("SELECT b FROM Board b WHERE b.content LIKE %:content% AND b.bno > 0 ORDER BY b.bno DESC")
+    List<Board> findByContent(@Param("content") String content);
+
+/*
+    Repository Entity 타입을 자동으로 사용
+    @Query("SELECT b FROM #{#entityName} b WHERE b.writer LIKE %?1% AND b.bno > 0 ORDER BY b.bno DESC")
+    List<Board> findByWriter(String writer);
+*/
 
 }
--- springboot-jpql/src/test/java/jpql/boot/BootApplicationTests.java
+++ springboot-jpql/src/test/java/jpql/boot/BootApplicationTests.java
@@ -8,20 +8,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collection;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BootApplicationTests {
 
 	private Logger logger = LoggerFactory.getLogger(BootApplicationTests.class);
+
 	@Autowired
 	private BoardRepository boardRepo;
 
 	@Test
-	public void testInsert200(){
+	public void insert200(){
 		for(int i = 0; i <= 200; i++){
 			Board board = new Board();
 			board.setTitle("제목 ..."+i);
@@ -32,17 +38,68 @@ public class BootApplicationTests {
 	}
 
 	@Test
-	public void testByTitle(){
+	public void findByTitle(){
 		boardRepo.findBoardByTitle("제목...177")
 				.forEach(board -> logger.info("### START BY TITLE " + board));
 	}
 
 	@Test
-	public void testByWriter(){
+	public void findByWriter(){
 		Collection<Board> results = boardRepo.findByWriter("user00");
 		results.forEach(
 				board -> logger.info("### START BY WRITER " + board)
 		);
+	}
+
+	@Test
+	public void writerContaining(){
+		Collection<Board> result = boardRepo.findByWriterContaining("05");
+		result.forEach(board -> System.out.println(board));
+	}
+
+	@Test
+	public void writerContainingOrTitleContainingLike(){
+		Collection<Board> result = boardRepo.findByWriterContainingOrTitleContaining("05", "%2%");
+		result.forEach(board -> System.out.println(board));
+	}
+
+	@Test
+	public void titleAndBno(){
+		Collection<Board> result = boardRepo.findByTitleContainingAndBnoGreaterThan("5",50L);
+		result.forEach(board -> System.out.println(board));
+	}
+
+	@Test
+	public void bnoGreaterThanOrderByBnoDesc(){
+		Collection<Board> result = boardRepo.findByBnoGreaterThanOrderByBnoDesc(90L);
+		result.forEach(board -> System.out.println(board));
+	}
+
+	@Test
+	public void bnoGreaterThanOrderByBnoDescPaging(){
+		Pageable paging = PageRequest.of(0, 10, Sort.Direction.ASC, "bno");
+		Collection<Board> result = boardRepo.findByBnoGreaterThanOrderByBnoDesc(0L, paging);
+		result.forEach(board -> System.out.println(board));
+	}
+
+	@Test
+	public void bnoPagingSort(){
+		Pageable paging = PageRequest.of(0, 10, Sort.Direction.ASC, "bno");
+		Page<Board> result = boardRepo.findByBnoGreaterThan(0L, paging);
+		System.out.println("PAGE SiZE: " + result.getSize());
+		System.out.println("TOTAL PAGES: " + result.getTotalPages());
+		System.out.println("TOTAL COUNT: " + result.getTotalElements());
+		System.out.println("NEXT: " + result.nextPageable());
+
+		// 다음 페이지는 어떻게 ?
+		List<Board> list = result.getContent();
+		list.forEach(board -> System.out.println(board));
+	}
+
+	@Test
+	public void findByTitleQuery(){
+		boardRepo.findByTitle("17")
+				.forEach(board -> System.out.println(board));
 	}
 
 }
